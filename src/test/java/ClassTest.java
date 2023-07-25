4cfcaf123d81cb999dd29359667e3d6c1d11e192
kravmaxim@gmail.com <Cegthgfhjkm!666>
2023-07-25 07:54:55 +0300

    Initial commit

---
+++ src/test/java/ClassTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ClassTest {
+
+    @Test
+    void testNotFail(){
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void testFail(){
+        Assertions.assertTrue(false);
+    }
+
+}
